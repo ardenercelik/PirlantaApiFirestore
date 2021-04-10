1cecbb8e253cb77dc9bc7d8987f87282baac7f12
Arden <ardenerc@gmail.com>
2021-04-10 22:22:07 +0300

    Hatalı routler için wildcard controller eklendi

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PirlantaApi.Controllers
+{
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{*url}")]
+        public IActionResult CatchAll() => NotFound();
+    }
+}
