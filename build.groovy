576b0677c57ebd6ec66c0fb0cfe2d936c35aef70
ardenercelik <58525494+ardenercelik@users.noreply.github.com>
2021-07-12 23:54:35 +0300

    Update build.groovy

--- build.groovy
+++ build.groovy
@@ -1,13 +1,24 @@
 pipeline {
     agent any
-    environment { 
-        CC = 'clang'
-    }
     stages {
     stage ('Clean workspace') {
       steps {
         cleanWs()
       }
-    }   
+    }
+    stage ('Git Checkout') {
+      steps {
+        git branch: 'test', credentialsId: 'arden-github', url: 'https://github.com/ardenercelik/PirlantaApiFirestore'
+      }
+    }
+    stage('Restore packages') {
+      dotnet_build()
+    }
+    }
+}
+
+def dotnet_build() {
+  dir($WORKSPACE) {
+        bat(script: 'dotnet restore PirlantaApi.csproj &&dotnet build PirlantaApi.csproj', returnStdout: true)
   }
 }
